ac57f18fe837c8f46e74d1b0d62c42803d414318
Joalor64 <rhgarcon@gmail.com>
2026-01-08 21:21:54 -0500

    new mod data thing wip

---
+++ mods.js
@@ -0,0 +1,49 @@
+// this file will replace modData.json, but it will be kept for backwards compatibility with older builds
+// testing for now
+modsList = [
+    {
+        name: "Example",
+        desc: "Description goes here.",
+        imgBG: "mod-icon",
+        imgIcon: "mod-background",
+        preview: "https://joalor64gh.github.io/",
+        downloadPC: "https://joalor64gh.github.io/foreverbox.html",
+        downloadAndroid: "https://joalor64gh.github.io/foreverbox.html",
+    },
+    {
+        name: "Example",
+        desc: "Description goes here.",
+        imgBG: "mod-icon",
+        imgIcon: "mod-background",
+        preview: "https://joalor64gh.github.io/",
+        downloadPC: "https://joalor64gh.github.io/foreverbox.html",
+        downloadAndroid: "https://joalor64gh.github.io/foreverbox.html",
+    },
+    {
+        name: "Example",
+        desc: "Description goes here.",
+        imgBG: "mod-icon",
+        imgIcon: "mod-background",
+        preview: "https://joalor64gh.github.io/",
+        downloadPC: "https://joalor64gh.github.io/foreverbox.html",
+        downloadAndroid: "https://joalor64gh.github.io/foreverbox.html",
+    },
+    {
+        name: "Example",
+        desc: "Description goes here.",
+        imgBG: "mod-icon",
+        imgIcon: "mod-background",
+        preview: "https://joalor64gh.github.io/",
+        downloadPC: "https://joalor64gh.github.io/foreverbox.html",
+        downloadAndroid: "https://joalor64gh.github.io/foreverbox.html",
+    },
+    {
+        name: "Example",
+        desc: "Description goes here.",
+        imgBG: "mod-icon",
+        imgIcon: "mod-background",
+        preview: "https://joalor64gh.github.io/",
+        downloadPC: "https://joalor64gh.github.io/foreverbox.html",
+        downloadAndroid: "https://joalor64gh.github.io/foreverbox.html",
+    }
+];
